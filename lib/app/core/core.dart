f08d20f55ee07685492858df27121b2877d28216
totozamany <jtotozamany73@gmail.com>
2022-10-17 08:19:33 +0300

    finish

--- lib/app/core/core.dart
+++ lib/app/core/core.dart
@@ -1,7 +1,7 @@
 // Constants Exports
 
 // Dto Exports
-
+export 'dto/auth/sign_in_dto.dart';
 // Errors Exports
 export 'errors/failures.dart';
 export 'errors/exceptions.dart';
--- test/unit-tests/app/core/dto/auth/sign_in_dto_test.dart
+++ test/unit-tests/app/core/dto/auth/sign_in_dto_test.dart
@@ -1,14 +1,29 @@
-import 'package:familys/app/core/dto/auth/sign_in_dto.dart';
+import 'dart:convert';
+
+import 'package:familys/app/core/core.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../../../fixtures/fixtures_reader.dart';
+
 void main() {
-  final tSignInDto = SignInDTO(email: 'test@gmail.com', password: '123456');
+  const tSignInDto = SignInDTO(email: 'test@gmail.com', password: '12345678');
 
   test('should return a valid SignInDTO from Json', () async {
     // arrange
-
+    final Map<String, dynamic> jsonMapSignInDto =
+        json.decode(fixture('sign_in_dto.json'));
     // act
-
+    final result = SignInDTO.fromJson(jsonMapSignInDto);
     // assert
+    expect(result, tSignInDto);
+  });
+
+  test('should return a JSON map containing the proper data', () async {
+    // arrange
+    final result = tSignInDto.toJson();
+    // act
+    final expectedMap = json.decode(fixture('sign_in_dto.json'));
+    // assert
+    expect(result, expectedMap);
   });
 }
